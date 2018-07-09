9b3e8de215c6da81c997c319fb38816438be403f
Alexander Kharichev <myx@myx.co.nz>
2018-07-09 17:46:58 +0300

    * build stage changed

--- make-prepare/builders/2201-ae3-packages.sh
+++ make-prepare/builders/2201-ae3-packages.sh
@@ -1,18 +1,16 @@
+Require ListChangedSourceProjects
+Require ListProjectProvides
 
-if [ "`type -t ListChangedSourceProjects`" != "function" ] ; then
-. "$MMDAPP/source/myx/myx.distro-prepare/sh-scripts/list-changed-source-projects.sh"
-fi
-if [ "`type -t ListCachedProjectProvides`" != "function" ] ; then
-. "$MMDAPP/source/myx/myx.distro-prepare/sh-scripts/list-cached-project-provides.sh"
-fi
+MDSC_SOURCE="${MDSC_SOURCE:-$MMDAPP/cache/sources}"
+MDSC_OUTPUT="${MDSC_OUTPUT:-$MMDAPP/output/distro}"
 
 MakeCachedProjectAe3Packages(){
-	local PKG="${1#$MMDAPP/source/}"
+	local PKG="$1"
 	if [ -z "$PKG" ] ; then
 		echo "MakeCachedProjectAe3Packages: 'PKG' argument is required!" >&2 ; exit 1
 	fi
 
-	local DIR="$MMDAPP/cached/source/$PKG"
+	local DIR="$MDSC_SOURCE/$PKG"
 	local SRC="$DIR/ae3-packages"
 	if test ! -d "$SRC" ; then
 		echo "Project doesn't have ae3-packages" >&2
@@ -28,7 +26,7 @@ MakeCachedProjectAe3Packages(){
 	for AE3PKG in $AE3PKGS ; do
 		if test -f "$AE3PKG/package.json" ; then
 			echo "tarring: $AE3PKG..." >&2
-			( cd "$AE3PKG" ; Async "`basename "$AE3PKG"`" tar -pczv -f "$MMDAPP/output/distro/$PKG/`basename "$AE3PKG"`.tar.gz" * ; wait ) &
+			( cd "$AE3PKG" ; Async "`basename "$AE3PKG"`" tar -pczv -f "$MDSC_OUTPUT/$PKG/`basename "$AE3PKG"`.tar.gz" * ; wait ) &
 		else
 			echo "skipping: $AE3PKG, no package.json" >&2
 		fi
@@ -38,9 +36,9 @@ MakeCachedProjectAe3Packages(){
 }
 
 MakeChangedAe3Packages(){
-	for PKG in `ListChangedSourceProjects` ; do
-#		if test ! -z "`ListCachedProjectProvides "$PKG" "build-prepare" | grep -e "^ae3-packages$"`" ; then
-			local CHECKDIR="$MMDAPP/cached/source/$PKG/ae3-packages"
+	for PKG in $( ListChangedSourceProjects ) ; do
+#		if test ! -z "$( ListProjectProvides "$PKG" "build-prepare" | grep -e "^ae3-packages$" )" ; then
+			local CHECKDIR="$MDSC_SOURCE/$PKG/ae3-packages"
 			if test -d "$CHECKDIR" ; then
 				Async -2 MakeCachedProjectAe3Packages "$PKG"
 				wait
