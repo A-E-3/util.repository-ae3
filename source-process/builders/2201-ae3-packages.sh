72011015c098177298087292b78b48f738800050
Alexander Kharichev <myx@myx.co.nz>
2018-07-20 22:19:19 +0300

    * variable name

--- source-process/builders/2201-ae3-packages.sh
+++ source-process/builders/2201-ae3-packages.sh
@@ -5,12 +5,12 @@ MDSC_SOURCE="${MDSC_SOURCE:-$MMDAPP/cache/sources}"
 MDSC_OUTPUT="${MDSC_OUTPUT:-$MMDAPP/output/distro}"
 
 MakeCachedProjectAe3Packages(){
-	local PKG="$1"
-	if [ -z "$PKG" ] ; then
-		echo "MakeCachedProjectAe3Packages: 'PKG' argument is required!" >&2 ; exit 1
+	local projectName="$1"
+	if [ -z "$projectName" ] ; then
+		echo "MakeCachedProjectAe3Packages: 'projectName' argument is required!" >&2 ; exit 1
 	fi
 
-	local DIR="$MDSC_SOURCE/$PKG"
+	local DIR="$MDSC_SOURCE/$projectName"
 	local SRC="$DIR/ae3-packages"
 	if [ ! -d "$SRC" ] ; then
 		echo "Project doesn't have ae3-packages" >&2 ; return 1
@@ -25,7 +25,7 @@ MakeCachedProjectAe3Packages(){
 	for AE3PKG in $AE3PKGS ; do
 		if test -f "$AE3PKG/package.json" ; then
 			echo "tarring: $AE3PKG..." >&2
-			( cd "$AE3PKG" ; Async "`basename "$AE3PKG"`" tar -pczv -f "$MDSC_OUTPUT/$PKG/`basename "$AE3PKG"`.tar.gz" * ; wait ) &
+			( cd "$AE3PKG" ; Async "`basename "$AE3PKG"`" tar -pczv -f "$MDSC_OUTPUT/$projectName/`basename "$AE3PKG"`.tar.gz" * ; wait ) &
 		else
 			echo "skipping: $AE3PKG, no package.json" >&2
 		fi
@@ -35,11 +35,11 @@ MakeCachedProjectAe3Packages(){
 }
 
 MakeChangedAe3Packages(){
-	for PKG in $( ListChangedSourceProjects ) ; do
-#		if test ! -z "$( ListProjectProvides "$PKG" "source-process" | grep -e "^ae3-packages$" )" ; then
-			local CHECKDIR="$MDSC_SOURCE/$PKG/ae3-packages"
+	for projectName in $( ListChangedSourceProjects ) ; do
+#		if test ! -z "$( ListProjectProvides "$projectName" "source-process" | grep -e "^ae3-packages$" )" ; then
+			local CHECKDIR="$MDSC_SOURCE/$projectName/ae3-packages"
 			if test -d "$CHECKDIR" ; then
-				Async -2 MakeCachedProjectAe3Packages "$PKG"
+				Async -2 MakeCachedProjectAe3Packages "$projectName"
 				wait
 			fi
 #		fi
