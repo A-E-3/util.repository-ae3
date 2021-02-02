e103011363c34bc6ed9a9e19c6f76b9dd22d1785
Alexander I. Kharitchev <myx@myx.co.nz>
2021-02-02 12:58:39 +0200

    - actions: unneeded header

--- actions/ae3/repository/sync-sources-git.sh
+++ actions/ae3/repository/sync-sources-git.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [ -z "$MMDAPP" ] ; then
-	set -e
-	export MMDAPP="$( cd $(dirname "$0")/../../../../../.. ; pwd )"
-	echo "$0: Working in: $MMDAPP"  >&2
-	[ -d "$MMDAPP/source" ] || ( echo "ERROR: expecting 'source' directory." >&2 && exit 1 )
-fi
+[ -d "$MMDAPP/source" ] || ( echo "ERROR: expecting 'source' directory." >&2 && exit 1 )
 
 . "$MMDAPP/source/ae3/util.repository-ae3/sh-scripts/sync-all-sources-ae3.sh"
