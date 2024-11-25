d8339e2425bf3313ab63faa5c31634477eddf2d5
Alex Kharichev <myx@myx.nz>
2024-11-25 17:21:29 +0200

    + more repos

--- sh-scripts/sync-all-sources-ae3.sh
+++ sh-scripts/sync-all-sources-ae3.sh
@@ -32,6 +32,9 @@ async "ae3/ae3-plug-iface-ssh/"					"git@github.com:A-E-3/ae3-plug-iface-ssh.git
 async "ae3/ae3.sdk-lang.acm-tpl/"				"git@github.com:A-E-3/ae3.sdk-lang.acm-tpl.git"
 async "ae3/ae3.sys.pkg.base/"					"git@github.com:A-E-3/ae3.sys.pkg.base.git"
 
+async "ae3/ae3.sys.pkg.i3.net/"					"git@github.com:A-E-3/ae3.sys.pkg.i3.net.git"
+async "ae3/ae3.sys.pkg.i3.web/"					"git@github.com:A-E-3/ae3.sys.pkg.i3.web.git"
+
 async "ae3/ae3.pkg.tgt.xml"						"git@github.com:A-E-3/ae3.pkg.tgt.xml.git"
 
 wait
